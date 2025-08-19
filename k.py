38d29cb9bfe15b43e5dd667835d9e3658f7de6b5
Niranjan164 <kniranjankumar164@gmail.com>
2025-08-19 20:29:00 +0530

    k.py

---
+++ k.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    print(f"{a}+{b}={a+b}")
+def sub(a,b):
+    print(f"{a}-{b}={a-b}")                                                                                          
+def multi(a,b):
+    print(f"{a}X{b}={a*b}")
+def div(a,b):
+    if b!=0 :
+        print("{a}/{b}={a/b}")
+    else :
+        print("Division is not possible\n")
